52d92e1f096e8a5e4098189c19f1633bb5288aff
saurabhkushwahha <saurabhkushwahhaa@gmail.com>
2024-11-12 23:49:32 +0530

    hero.tsx updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,14 +7,14 @@ const Hero = () => {
       <div className="hero-map" />
 
       <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
-        <Image 
+        <Image
           src="/camp.svg"
           alt="camp"
           width={50}
           height={50}
           className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
         />
-        <h1 className="bold-52 lg:bold-88">Putuk Truno Camp Area</h1>
+        <h1 className="bold-52 lg:bold-88">Aniket Truno Camp Area</h1>
         <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
           We want to be on each of your journeys seeking the satisfaction of seeing the incorruptible beauty of nature. We can help you on an adventure around the world in just one app
         </p>
@@ -22,7 +22,7 @@ const Hero = () => {
         <div className="my-11 flex flex-wrap gap-5">
           <div className="flex items-center gap-2">
             {Array(5).fill(1).map((_, index) => (
-              <Image 
+              <Image
                 src="/star.svg"
                 key={index}
                 alt="star"
@@ -39,16 +39,16 @@ const Hero = () => {
         </div>
 
         <div className="flex flex-col w-full gap-3 sm:flex-row">
-          <Button 
-            type="button" 
-            title="Download App" 
-            variant="btn_green" 
+          <Button
+            type="button"
+            title="Download App"
+            variant="btn_green"
           />
-          <Button 
-            type="button" 
-            title="How we work?" 
+          <Button
+            type="button"
+            title="How we work?"
             icon="/play.svg"
-            variant="btn_white_text" 
+            variant="btn_white_text"
           />
         </div>
       </div>
